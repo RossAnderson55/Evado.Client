2aa0d83e9e79935c325c9d29e538b74f4d058c33
Ross Anderson <ross@evado.com>
2022-08-23 15:43:38 +1000

    Downgrade .NET vfrom 4.8 to 4.0

--- evado.uniform.webclient/default.aspx.cs
+++ evado.uniform.webclient/default.aspx.cs
@@ -21,7 +21,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Configuration;
+using System.Configuration; 
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Data;
