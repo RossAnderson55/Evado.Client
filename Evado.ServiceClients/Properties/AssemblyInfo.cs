2914160e407fcd6873553acccb29e7ad440ec97a
Ross Anderson <ross@evado.com>
2025-09-09 15:45:56 +1000

    Release version 6.5.0

--- Evado.ServiceClients/Properties/AssemblyInfo.cs
+++ Evado.ServiceClients/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion ( "6.4.4" )]
-[assembly: AssemblyFileVersion ( "6.4.4" )]
+[assembly: AssemblyVersion ( "6.5.0" )]
+[assembly: AssemblyFileVersion ( "6.5.0" )]
